fda906b4cc075afaa569a37b5ae63adb761a9ba0
baptill <baptiste.lmn@gmail.com>
2025-08-28 07:56:22 +0200

    add some parameters to lbc

--- lbc-finder/config.py
+++ lbc-finder/config.py
@@ -4,14 +4,13 @@ import lbc
 
 def handle(ad: lbc.Ad, search_name: str):
     print(f"Nouvelles annonces pour {search_name}")
-    # print(f"Title : {ad.subject}")
-    # print(f"Price : {ad.price} €")
-    # print(f"URL : {ad.url}")
-    # print(f"status : {ad.status}")
-    # print(f"category_name : {ad.category_name}")
+    print(f"Title : {ad.subject}")
+    print(f"Price : {ad.price} €")
+    print(f"URL : {ad.url}")
+    print(f"status : {ad.status}")
+    print(f"category_name : {ad.category_name}")
     print(f"location : {ad.location}")
-    # print(f"Body : {ad.body}")
-    # print("-" * 40)
+    print(f"Body : {ad.body}")
 
 
 location = lbc.City(lat=48.6846, lng=-1.3572, radius=50_000, city="Avranches")  # 10 km
